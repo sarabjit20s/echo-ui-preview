53fb6d950578c08a1351a17e9602a5f116c4e4b8
Sarabjit Singh <heysarabjit@gmail.com>
2024-12-04 18:16:07 +0530

    fix: status bar color in dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
+import { StatusBar } from 'react-native';
 import { Stack } from 'expo-router';
 
 import '@/styles/unistyles';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }} />
+      <StatusBar backgroundColor="transparent" translucent />
+    </>
+  );
 }
