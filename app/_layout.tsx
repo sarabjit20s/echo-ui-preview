9862c2011c652111ccdeb95810d422d133955418
Sarabjit Singh <heysarabjit@gmail.com>
2024-12-05 19:08:09 +0530

    chore: add PortalProvider in the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,17 +3,20 @@ import { Stack } from 'expo-router';
 import { UnistylesProvider } from 'react-native-unistyles';
 
 import '@/styles/unistyles';
+import { PortalProvider } from '@/utils/portal';
 
 export default function RootLayout() {
   return (
     <UnistylesProvider>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          animation: 'slide_from_right',
-        }}
-      />
-      <StatusBar backgroundColor="transparent" translucent />
+      <PortalProvider>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_right',
+          }}
+        />
+        <StatusBar backgroundColor="transparent" translucent />
+      </PortalProvider>
     </UnistylesProvider>
   );
 }
